74a1d4964880092225119fe38f80855cd7bbb9d9
Dania Duany <deduany@gmail.com>
2020-05-04 19:19:45 +0200

    feature/REV-12-balance-component
    Create Dashboard file components:
    * Dashboard.js
    * Dashboard.test.js
    * Dashboard.module.css
    * Dashboard.stories.js
    
    Create Balance story:
     Import react
     Import Balance component
     Add the export default
     Add function to display Balance
    
    Reported-By: Dania Duany

---
+++ src/components/pages/Dashboard/Balance/Balance.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./Balance.module.scss";
+
+export function Balance() {
+
+
+
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
---
+++ src/components/pages/Dashboard/Dashboard.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Balance} from './Balance/Balance'
+
+
+export default {
+    component: Balance,
+    title: 'Account Balance'
+}
+
+export const standard = () => {
+    return (
+        <Balance/>
+    )
+}
