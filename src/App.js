f340702fb22d13fc897becb943715b980c8871a3
Dania Duany <deduany@gmail.com>
2020-05-04 18:48:41 +0200

    feature/REV-12-balance-component
    Add files for Balance component:
    
     * Balance.js
     * Balance.module.scss
     * Balance.stories.js
     * Balance.test.js
    
     Reported-By: Dania Duany

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-
+           <img src="https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
     </div>
   );
 }
