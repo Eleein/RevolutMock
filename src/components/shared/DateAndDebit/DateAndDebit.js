eb5eb24fee395fe24f7a0354c40bf289152d4a1f
Dania Duany <deduany@gmail.com>
2020-05-25 11:35:38 +0200

    feature/Rev-4/Display-Date-and-Debit 	*Create component files: 		-DateAndDebit.js 		-DateAndDebit module scss 		-DateAndDebit test file 		-DateAndDebit storybook Reported-By: Dania Duany

---
+++ src/components/shared/DateAndDebit/DateAndDebit.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './DateAndDebit.module.scss';
+
+
+export function DateAndDebit(){
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
---
+++ src/components/shared/DateAndDebit/DateAndDebit.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {DateAndDebit} from "./DateAndDebit";
+
+
+export default {
+    component: DateAndDebit,
+    title: "Account Date and Debit"
+}
+
+
+ export const standard = () => {
+    return (<DateAndDebit/>)
+}
