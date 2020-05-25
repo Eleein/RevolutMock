8bd9c015c6fdef8713b0c48fab4d45ef936ec573
Dania Duany <deduany@gmail.com>
2020-05-25 15:26:11 +0200

    feature/Rev-4/Display-Date-and-Debit 	Request PR changes: 		-Separate layout and style 		-Create layout classes in theme 		-Use font size from theme 		-Add standard border in theme
    Reported-by: Dania Duany

--- src/components/shared/DateAndDebit/DateAndDebit.js
+++ src/components/shared/DateAndDebit/DateAndDebit.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./DateAndDebit.module.scss";
 import format from "date-fns/format";
+import cx from "classnames";
 
 export function DateAndDebit({ date, debit, currencySymbol }) {
   const givenDate = format(date, "d, MMMM yyyy");
 
   return (
-    <div className={styles.layoutDateAndDebit}>
+    <div className={cx(styles.layoutDateAndDebit, styles.stylesDateAndDebit)}>
       <div className={styles.date}>{givenDate}</div>
       <div className={styles.debit}>
-        {" "}
         - {currencySymbol}
         {debit}
       </div>
