20334bba45c6c12dab67014a326356da27375268
Dania Duany <deduany@gmail.com>
2020-05-26 12:29:26 +0200

    feature/REV-3/Account-Activity-Graph
    	*Create new Branch Account-Activity-Graph
    	*Create component files:
    		-MonthSummaryInGraph.js
    		-MonthSummaryInGraph.module.scss
    		-MonthSummaryInGraph.stories.js
    		MonthSummaryInGraph.test.js
    
    Reported-by: Dania Duany

---
+++ src/components/pages/Dashboard/MonthSummaryInGraph/MonthSummaryInGraph.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./MonthSummaryInGraph.module.scss";
+
+export function MonthSummaryInGraph() {
+  return <div>I'm a pretty Jelly fish!</div>;
+}
---
+++ src/components/pages/Dashboard/MonthSummaryInGraph/MonthSummaryInGraph.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { MonthSummaryInGraph } from "./MonthSummaryInGraph";
+
+export default {
+  title: "Month summary",
+  component: MonthSummaryInGraph,
+};
+
+export const standard = () => {
+  return <MonthSummaryInGraph />;
+};
